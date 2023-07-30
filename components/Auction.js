e9e32bd45c9085e44980dcc116b38846033ff1e9
Tim <timstewartj@gmail.com>
2023-07-30 09:10:55 -0700

    fetch and cache auction items

--- components/Auction.js
+++ components/Auction.js
@@ -1,13 +1,50 @@
 import { Text, View } from 'react-native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { shape, string } from 'prop-types';
+import axios, { toFormData } from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function Auction({ auction }) {
+  const [items, setItems] = useState(null);
+
+  const itemsKey = `auction-${auction.id}`;
+
+  useEffect(() => {
+    const checkAndFetchItems = async () => {
+      // check if we have the items for this auction in storage
+      const itemData = JSON.parse(await AsyncStorage.getItem(itemsKey));
+      if (itemData == null) {
+        // fetch from API if not in storage
+        axios.post('https://www.bidrl.com/api/getitems', toFormData({
+          auction_id: auction.id,
+          'filters[perpage]': '120',
+          item_type: 'itemlist',
+          lotnum: '',
+          seqnum: '',
+          close_groups: '',
+          show_closed: 'closeed',
+          perpetual: '',
+        })).then((res) => {
+          console.log(res.data);
+          // save the items
+          AsyncStorage.setItem(itemsKey, JSON.stringify(res.data));
+          setItems(res.data);
+        });
+      } else {
+        setItems(itemData);
+      }
+    };
+    checkAndFetchItems();
+  }, []);
+
   return (
     <View>
       <Text>
         {auction.title}
       </Text>
+      <View>
+        {items !== null ? items.items.map((item) => <Text key={item.id}>{item.title}</Text>) : 'Loading...'}
+      </View>
     </View>
   );
 }
