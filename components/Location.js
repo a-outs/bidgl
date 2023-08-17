1c6e230219c3fda1b67a74e88116244d712a5fe3
Tim Stewart <timstewartj@gmail.com>
2023-08-17 11:11:49 -0700

    use pressable for location grid

--- components/Location.js
+++ components/Location.js
@@ -1,6 +1,6 @@
 import { arrayOf, func, shape } from 'prop-types';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Pressable, Text, View } from 'react-native';
 
 export default function Location({ location, onLocationCheck, checkedAffiliates }) {
   const checked = checkedAffiliates.some((a) => a.id === location.id);
@@ -11,17 +11,17 @@ export default function Location({ location, onLocationCheck, checkedAffiliates 
   };
 
   return (
-    <View style={{ backgroundColor: checked ? 'gray' : 'white' }}>
-      <label htmlFor={location.url}>
-        <input
-          type="checkbox"
-          checked={checkedAffiliates.some((a) => a.id === location.id)}
-          onChange={handleChange}
-          id={location.url}
-          style={{ display: 'none' }}
-        />
-        <Text>{location.name}</Text>
-      </label>
+    <View>
+      <Pressable
+        style={({ pressed }) => [
+          {
+            backgroundColor: pressed ? 'rgb(210, 230, 255)' : 'white',
+          },
+        ]}
+        onPress={handleChange}
+      >
+        <Text style={{ color: checked ? 'red' : 'black' }}>{location.name}</Text>
+      </Pressable>
     </View>
   );
 }
